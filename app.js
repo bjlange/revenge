23b276466101e7747526a5c3a1fd17a3774556c0
Brian Lange <brian.lange@datascopeanalytics.com>
2014-08-15 00:05:56 -0500

    Add team tracking to show distributions of player need during my picks

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
         this.positions = ['QB','RB','WR','TE'];
         this.user_initial = 3;
         this.current_pick = 1;
+        this.situations = {};
         this.round_to_pick = function(initial, round) {
             var pick = -1;
             if(round % 2 == 0) {
@@ -14,36 +15,102 @@
             }
             return pick;
         };
+        this.pick_array = Array(8).join('ABCDEFGHIJKLLKJIHGFEDCBA');
+
+        this.team_tallies = {};
+        this.team_projected = {};
+        this.setup_teams = function() {
+            for(i=0;i < 12;i++){
+                this.team_tallies[this.pick_array[i]] = {QB:0,
+                                                         WR:0,
+                                                         RB:0,
+                                                         TE:0,
+                                                         DEF:0,
+                                                         K:0};
+                this.team_projected[this.pick_array[i]] = {QB:0,
+                                                           WR:0,
+                                                           RB:0,
+                                                           TE:0};
+            }
+            for(i=0; i<this.positions.length;i++){
+                this.situations[this.positions[i]] = {};
+            }
+        }
+        this.update_team_situations = function() {
+            var teams_before_next_pick = this.teams_before_next_pick;
+            var unique_teams = {};
+            this.situations = {};
+            for(var i=0; i<teams_before_next_pick.length; i++) {
+                unique_teams[teams_before_next_pick[i]] = true;
+            }
+            for(var i=0; i<this.positions.length; i++) {
+                var position =  this.positions[i];
+                this.situations[position] = {}
+
+                for(var team in unique_teams) {
+                    var tally = this.team_tallies[team][position];
+                    var projected = this.team_projected[team][position];
+                    if(tally in this.situations[position]) {
+                        this.situations[position][tally].push(team + " (" + projected + ")");
+                    }
+                    else {
+                        this.situations[position][tally] = [team + " (" + projected + ")"];
+                    }
+                }
+            }
+        }
+        this.teams_before_next_pick = [];
+        this.update_teams_before_next_pick = function() {
+            this.teams_before_next_pick = this.pick_array.slice(this.current_pick+1,
+                                                                this.practical_next_pick()-1)};
         this.get_current_round = function() {
             return Math.floor((this.current_pick-1)/12)+1
         };
+
         this.get_round_pick = function() {
             var mod = this.current_pick % 12;
             if(mod === 0) {mod = 12};
             return mod;
         };
+
         this.submit_pick = function() {
-            this.current_pick += 1;
             var ind = this.all_players.indexOf(this.player_selected);
+            var projected = this.all_players[ind]['projected_pts'];
+            var position = this.all_players[ind]['pos'];
             this.all_players.splice(ind, 1);
+            this.team_tallies[this.pick_array[this.current_pick-1]][position] += 1;
+            this.team_projected[this.pick_array[this.current_pick-1]][position] += projected;
+            this.update_teams_before_next_pick();
+            this.update_team_situations();
+            this.current_pick += 1;
         };
         this.picking_now = function() {
             return(this.current_pick ===
                    this.round_to_pick(this.user_initial,
-                                      this.get_current_round()))
+                                      this.get_current_round()));
         }
         this.user_nplus_pick = function(n) {
-            var current_round = this.round_to_pick(this.user_initial,
-                                                   this.get_current_round());
+            var current_round_pick = this.round_to_pick(this.user_initial,
+                                                        this.get_current_round());
 
-            if(this.current_pick >= current_round) {
+            if(this.current_pick >= current_round_pick) {
                 return this.round_to_pick(this.user_initial,
-                                                this.get_current_round()+(n));
+                                          this.get_current_round()+(n));
             }
             else {
                 return this.round_to_pick(this.user_initial,
                                           this.get_current_round()+(n-1));
             }
+        }
+        this.practical_next_pick = function() {
+            // this is like user_nplus_pick, but it covers the corner
+            // cases where you're picking at the turn
+
+            var next_pick = this.user_nplus_pick(1);
+            if (next_pick === (this.current_pick+1)) {
+                next_pick = this.user_nplus_pick(2)
+            };
+            return next_pick;
         }
         this.player_list = function(position) {
             return this.all_players.filter(function(element) {
@@ -52,7 +119,7 @@
         };
 
         this.get_vorp = function(position) {
-            var next_pick = this.user_nplus_pick(1);
+            var next_pick = this.practical_next_pick();
             var top_this = this.player_list(position).sort(function(a, b){
                 return b['projected_pts'] - a['projected_pts'];
             });
@@ -62,6 +129,9 @@
             })[0]['projected_pts'];
 
             return top_score_now - top_next;
+        }
+        this.is_undervalued = function(adp) {
+            return (adp < this.current_pick);
         }
 
         this.all_players = players;
@@ -76,5 +146,6 @@
             this.user_initial = parseInt(prompt("Input your first round pick position:"));
         }
         this.set_initial_draft_position();
+        this.setup_teams();
     });
 })();
